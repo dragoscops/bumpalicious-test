48d2c09fa20cdc5689623ec2edc1812e247e1731
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-05-11 22:25:31 +0300

    chore: debug 1746991531

--- python-setup-py/setup.py
+++ python-setup-py/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="python-project-setup-py",
+    name="python-setup-py",
     version="1.0.0",
     packages=find_packages(),
     description="Test project for Bumpalicious GitHub Action",
